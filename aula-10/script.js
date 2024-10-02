bceb611bf412f7b758b81430479025d0ce68472e
imdarkcode <otavio.sudano13@gmail.com>
2024-10-02 17:53:55 -0300

    feat: add aula_10

---
+++ aula-10/script.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+
+    document.getElementById("formQuestion").onsubmit = function(evento) {
+        evento.preventDefault();
+
+        if (document.getElementById("selectQuestion").value == "1") {
+            document.getElementById("tableAnswers").innerHTML = '<tr style="font-size: 20px; height: 20px"> <b>Resposta 1:</b> O javascript é uma linguagem de programação de tipagem fraca, que pode ser utilizada para criar interações em páginas web deixando-as mais dinâmica. </tr>' + document.getElementById("tableAnswers").innerHTML;
+        }
+
+        else {
+            document.getElementById("tableAnswers").innerHTML = '<tr style="font-size: 20px; height: 20px"> <b>Resposta 2:</b> Os eventos de javascript são "sinais" que cada elemento html pode emitir, por exemplo: Ao clicar em um input de texto o elemento emitirá o evento "onFocus". </tr>' + document.getElementById("tableAnswers").innerHTML;
+        }
+    }
+}
