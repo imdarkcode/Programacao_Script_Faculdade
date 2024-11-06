7895a69fcecf5dbcfadb2121d3d3f0c346494fbd
imdarkcode <otavio.sudano13@gmail.com>
2024-11-06 17:51:13 -0300

    feat: update projeto

--- projeto/script.js
+++ projeto/script.js
@@ -1,7 +1,32 @@
 class Usuario {
-    constructor(nome, email, senha) {
+    constructor(nome, telefone, email, senha) {
         this.nome = nome;
+        this.telefone = telefone;
         this.email = email;
         this.senha = senha;
     }
 }
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        nome: '',
+        telefone: '',
+        email: '',
+        senha: '',
+        listaUsuarios:[] 
+    },
+    methods: {
+        verificarCampos: function() {
+            return (this.nome != '' && this.telefone != '' && this.email != '' && this.senha != '');
+        },
+        cadastrar: function(){
+            let novoUsuario = new Usuario(this.nome, this.telefone, this.email, this.senha);
+            this.listaUsuarios.push(novoUsuario);
+            this.nome = '';
+            this.telefone = '';
+            this.email = '';
+            this.senha = '';
+        },
+    }
+});
