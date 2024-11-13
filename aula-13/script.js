fa23bf3098347d47dcd0367a34ae06e49c245bdd
imdarkcode <otavio.sudano13@gmail.com>
2024-11-13 17:49:32 -0300

    feat: add aula_13

---
+++ aula-13/script.js
@@ -0,0 +1,7 @@
+// $ = jQuery
+
+$(document).ready(function(){ // Executar função quando o documento estiver pronto
+    $('#btnCarregar').click(function() { // Executar função ao clicar no botão
+        alert('Hello world');
+    });
+});
