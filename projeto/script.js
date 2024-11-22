6a580f53b9c82764471551d8718ebf99ddbef4c1
imdarkcode <otavio.sudano13@gmail.com>
2024-11-22 13:34:12 -0300

    feat: update projeto

--- projeto/script.js
+++ projeto/script.js
@@ -14,19 +14,41 @@ var app = new Vue({
         telefone: '',
         email: '',
         senha: '',
-        listaUsuarios:[] 
+        listaUsuarios:[],
+        hora: '00:00:00',
+        posicao: -1
     },
     methods: {
         verificarCampos: function() {
             return (this.nome != '' && this.telefone != '' && this.email != '' && this.senha != '');
         },
-        cadastrar: function(){
+        salvar: function(){
             let novoUsuario = new Usuario(this.nome, this.telefone, this.email, this.senha);
-            this.listaUsuarios.push(novoUsuario);
+            if (this.posicao == -1) {
+                this.listaUsuarios.push(novoUsuario);
+            } else {
+                Vue.set(this.listaUsuarios, this.posicao, novoUsuario);
+            }
             this.nome = '';
             this.telefone = '';
             this.email = '';
             this.senha = '';
+            this.posicao = -1;
         },
+        carregarHora: function() {
+            axios.get('http://date.jsontest.com/').then (res => (this.hora = res.data.time));
+        },
+        alterar: function(index) {
+            this.nome = this.listaUsuarios[index].nome;
+            this.telefone = this.listaUsuarios[index].telefone;
+            this.email = this.listaUsuarios[index].email;
+            this.senha = this.listaUsuarios[index].senha;
+            this.posicao = index;
+        },
+        excluir: function(index) {
+            if (confirm('Deseja excluir?')) {
+                this.listaUsuarios.splice(index, 1);
+            }
+        }
     }
 });
